19b43482ea0a0af8e96d0b65e1238994aaeb1be7
Anton Vidinev <anton.vidinev@da-14.com>
2017-10-05 21:06:37 +0300

    Add logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+/**
+ * Remove CDN icons make icon font
+ * Add translation module
+ * SSR + AOT
+ * Redux
+ * Create icons image sprite
+ * */
+
 @Component({
   selector: 'vk-root',
   templateUrl: './app.component.html',
